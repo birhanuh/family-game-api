bb2f2d85ac32beab7b2f032599e37d8a5b169478
Birhanu Hailemariam <birhanu.hailemariam@gmail.com>
2021-04-15 23:07:14 +0300

    Add cors middleware and declaration.

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const games = require('./controllers/games');
 
 app.use(express.json());
 
+// CORS
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
